97a129586eda9911f244ee15799482c1f22108a0
meenakshi.paryani@sjsu.edu <meenakshi.paryani@sjsu.edu>
2017-09-25 18:54:05 -0700

    Sample JSX app

--- food-cart/src/App.js
+++ food-cart/src/App.js
@@ -5,17 +5,25 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      element
     );
   }
 }
+
+function formatName(user) {
+  return user.firstName + ' ' + user.lastName;
+}
+
+const user = {
+  firstName: 'Harper',
+  lastName: 'Perez'
+};
+
+const element = (
+  <h1>
+    Hello, {formatName(user)}!
+  </h1>
+);
+
 
 export default App;
