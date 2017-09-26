c154737b144233764021db498fdd3572a3e396ef
meenakshi.paryani@sjsu.edu <meenakshi.paryani@sjsu.edu>
2017-09-26 15:05:16 -0700

    Initial Setup for You Tube App

---
+++ react/ReduxSimpleStarter/src/index.js
@@ -0,0 +1,10 @@
+import React from 'React';
+import ReactDOM from 'react-dom';
+
+const API_KEY = 'AIzaSyAPoi8RJMC4X__bHDJGS92wddQKbZ-mEj8';
+
+const App = () => {
+  return <div>Hi!!</div>
+}
+
+ReactDOM.render(<App />, document.querySelector('.container'));
