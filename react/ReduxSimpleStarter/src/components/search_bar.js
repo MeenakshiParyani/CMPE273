6b21d998a86e2bbf7b5afef3e1c917974bbae233
meenakshi.paryani@sjsu.edu <meenakshi.paryani@sjsu.edu>
2017-09-26 16:31:38 -0700

    Event handling of html components

--- react/ReduxSimpleStarter/src/components/search_bar.js
+++ react/ReduxSimpleStarter/src/components/search_bar.js
@@ -1,8 +1,26 @@
 import React, {Component} from 'react';
 
 class SearchBar extends Component{
+  constructor(props){
+    super(props);
+    this.state = {term:''};
+    this.onInputChange = this.onInputChange.bind(this);
+  }
+
+
   render(){
-    return <input />;
+    return (
+      <div>
+        <input onChange={this.onInputChange}/>
+        <br></br>
+        Your input: {this.state.term}
+      </div>
+    );
+  }
+
+  onInputChange(event){
+    console.log(event.target.value);
+    this.setState({term: event.target.value});
   }
 
 }
