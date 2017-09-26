3b2d635d1c83829a514be5016bb3f765fe18a3f2
meenakshi.paryani@sjsu.edu <meenakshi.paryani@sjsu.edu>
2017-09-26 16:38:00 -0700

    Controlled Components

--- react/ReduxSimpleStarter/src/components/search_bar.js
+++ react/ReduxSimpleStarter/src/components/search_bar.js
@@ -14,6 +14,7 @@ class SearchBar extends Component{
         <input onChange={this.onInputChange}/>
         <br></br>
         Your input: {this.state.term}
+        <input value={this.state.term}/>
       </div>
     );
   }
